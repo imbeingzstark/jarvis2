944c31b0d2fe4734ae9cb4461938c6424dd1b31b
Raj <singhraj0206@yahoo.com>
2021-03-23 17:14:07 +0530

    modified

--- mydummyproject/src/mydummyproject/Hello.java
+++ mydummyproject/src/mydummyproject/Hello.java
@@ -6,9 +6,7 @@ public class Hello {
 		System.out.println("hello");
 		System.out.println("Hello raj!");
 		System.out.println("namaste");
-
 		System.out.println("namaste2");
-
 		System.out.println("new line");
 
 	}
